a3ba3d02ac69c96af455f10a76206510ba79966c
ive <ivetourinho@gmail.com>
2017-01-28 00:46:52 -0300

    initial header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 require('./style.scss');
 
 const Header = () => ( 
-  <div >
-  Test
-</div>
+  <div className="Header bgimg-1 w3-display-container w3-grayscale-min" >
+      <span className = " Span w3-jumbo w3-hide-small" >The greatest take away message!
+      </span>
+
+      <span className = " Span w3-xxlarge w3-hide-large w3-hide-medium">Another take away message for mobile users!
+      </span>
+
+      <br></br>
+
+      <span className = " Span w3-large">Here more details about what is worth to keep on mind regard to our product.
+      </span>
+
+  </div>
 );
 
 export default Header;
