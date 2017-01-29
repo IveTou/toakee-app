fb9bd3a73f39b6af02b8ab8bfd8e6a9a1ed2b737
ive <ivetourinho@gmail.com>
2017-01-29 01:55:21 -0300

    basic form without send message working

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -8,17 +8,20 @@ const Contact = () => (
     <p className="w3-center w3-large">Lets get in touch. Send us a message:</p>
 
       <div className="Container w3-row-padding">
-        <div className="w3-half">
+        <div className="w3-third">
         
           <p><i className="fa fa-map-marker fa-fw w3-xxlarge w3-margin-right"></i> Salvador, BA</p>
           <p><i className="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Phone: +55 71 151515</p>
           <p><i className="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email: contato@toakee.com</p>
-          <br></br>
-          
-          <Form/>
+          <br></br>    
       
         </div>
+
+        <div className="w3-twothird">
+          <Form/>
+        </div>
       </div>
+
 
   </div>
 );
