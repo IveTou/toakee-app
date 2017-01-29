551c1e83a338891637c12e39d8f951df98f423cc
ive <ivetourinho@gmail.com>
2017-01-29 03:58:33 -0300

    add id

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 require('./style.scss');
 
 const Contact = () => ( 
-  <div className="Contact w3-container w3-padding-24 w3-light-grey">
+  <div className="Contact w3-container w3-padding-24 w3-light-grey" id="contact">
     <h3 className="w3-center">CONTACT</h3>
     <p className="w3-center w3-large">Lets get in touch. Send us a message:</p>
 
