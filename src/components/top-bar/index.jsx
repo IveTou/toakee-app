824d15f7e9bf8dab4beb775ca695f6307bf93d47
ive <ivetourinho@gmail.com>
2017-01-29 17:52:05 -0300

    top-bar lint issues

--- src/components/top-bar/index.jsx
+++ src/components/top-bar/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import AppBar from 'react-toolbox/lib/app_bar';
 import Navigation from 'react-toolbox/lib/navigation';
-import Button from 'react-toolbox/lib/button';
 import ToakeeIcon from '~/src/components/icon';
 
 require('./style.scss');
 
 const TopBar = () => (
-  <AppBar title="Toakee" className="TopBar w3-top  w3-card-2" leftIcon={<ToakeeIcon/>}>
-    <Navigation type="horizonta"/>
+  <AppBar title="Toakee" className="TopBar w3-top  w3-card-2" leftIcon={<ToakeeIcon />}>
+    <Navigation type="horizonta" />
   </AppBar>
 );
 
