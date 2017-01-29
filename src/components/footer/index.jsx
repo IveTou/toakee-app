242d6268a70652137aa8d892f27190c05f9e2f2d
ive <ivetourinho@gmail.com>
2017-01-29 17:54:38 -0300

    footer lint issues

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Footer = () => 
+const Footer = () =>
   <footer className="Footer w3-center w3-black w3-padding-24">
-    <p>Powered by <a href="http://www.toakee.com" title="TOAKEE" target="_blank" className="w3-hover-text-indigo">toakee.com</a></p>
+    <p>Powered by <a href="http://www.toakee.com" title="TOAKEE" target="_blank" rel="noopener noreferrer" className="w3-hover-text-indigo">toakee.com</a></p>
   </footer>;
 
 export default Footer;
