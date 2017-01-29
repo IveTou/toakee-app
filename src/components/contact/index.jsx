4657028fe5796c19b69549a12ffa3f5c8ac19715
ive <ivetourinho@gmail.com>
2017-01-29 00:30:17 -0300

    first commit

---
+++ src/components/contact/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+require('./style.scss');
+
+const Contact = () => ( 
+  <div className="Contact w3-container w3-padding-24 w3-light-grey">
+    <h3 class="w3-center">CONTACT</h3>
+  <p class="w3-center w3-large">Lets get in touch. Send us a message:</p>
+  </div>
+);
+
+export default Contact;
