95d7a677f192a9b7a26d0865c2968709c174127e
ive <ivetourinho@gmail.com>
2017-01-29 03:54:01 -0300

    attempt to improve topbar

--- src/components/top-bar/index.jsx
+++ src/components/top-bar/index.jsx
@@ -8,14 +8,29 @@ require('./style.scss');
 
 
 const TopBar = () => (
-  <AppBar title="Toakee" className="TopBar w3-text-purple  w3-hover-yellow w3-card-2" leftIcon={<ToakeeIcon/>}>
+  <AppBar title="Toakee" className="TopBar w3-top  w3-card-2" leftIcon={<ToakeeIcon/>}>
     <Navigation type="horizontal">
+    {/*
       <RouterLink to={{ pathname: '/login' }}>
         <Button className ="w3-right w3-hide-small" label="Login" flat />
       </RouterLink>
       <Button className ="w3-right w3-hide-large w3-hide-medium" label="V" flat />
+      */}
     </Navigation>
+    
   </AppBar>
 );
+/*
+
+const TopBar = () => (
+  <div className="w3-top">
+    <ul className="TopBar w3-navbar w3-white w3-card-2" >
+      <li>
+        <a  className="w3-wide"><b>To</b>aki  <i className="fa fa-envelope"/></a>
+      </li>
+    </ul>
+  </div>
+);
+*/
 
 export default TopBar;
