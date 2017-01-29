0ff482ae7b911fdd90df1407fdbca80a9179cafd
ive <ivetourinho@gmail.com>
2017-01-29 16:53:23 -0300

    clean footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-require('./style.scss');
 
 const Footer = () => 
-  <footer className ="Footer w3-center w3-black w3-padding-24">
-    <p>Powered by <a href="http://www.toakee.com" title="W3.CSS" target="_blank" className="w3-hover-text-indigo">toakee.com</a></p>
+  <footer className="Footer w3-center w3-black w3-padding-24">
+    <p>Powered by <a href="http://www.toakee.com" title="TOAKEE" target="_blank" className="w3-hover-text-indigo">toakee.com</a></p>
   </footer>;
 
 export default Footer;
