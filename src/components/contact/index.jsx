893944df6abfb1b859fd63327a6ad581909530f2
ive <ivetourinho@gmail.com>
2017-01-29 00:33:33 -0300

    contact in the landing

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -3,8 +3,8 @@ require('./style.scss');
 
 const Contact = () => ( 
   <div className="Contact w3-container w3-padding-24 w3-light-grey">
-    <h3 class="w3-center">CONTACT</h3>
-  <p class="w3-center w3-large">Lets get in touch. Send us a message:</p>
+    <h3 className="w3-center">CONTACT</h3>
+  <p className="w3-center w3-large">Lets get in touch. Send us a message:</p>
   </div>
 );
 
