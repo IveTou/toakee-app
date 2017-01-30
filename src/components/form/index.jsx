99e98a3ce7c9b0f6455deaac9f861e589756dc07
ive <ivetourinho@gmail.com>
2017-01-30 11:40:12 -0300

    some adjusts

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -22,11 +22,13 @@ class Form extends React.Component {
   render() {
     return (
       <form className="Form" onSubmit={this.handleSubmit}>
-        <Input className="Input" type="email" label="E-mail" value={this.state.email} required onChange={this.handleChange.bind(this, 'email')} />
-        <Input className="Input" type="tel" label="Telefone" name="phone" value={this.state.phone} onChange={this.handleChange.bind(this, 'phone')} />
-        <Input className="Input" type="text" multiline required rows={4} label="Mensagem" maxLength={400} value={this.state.multiline} onChange={this.handleChange.bind(this, 'multiline')} />
+        <Input className="Input" type="email" label="E-mail" value={this.state.email} required onChange={() => { this.handleChange(this, 'email'); }} />
+        <Input className="Input" type="tel" label="Telefone" name="phone" value={this.state.phone} onChange={() => { this.handleChange(this, 'phone'); }} />
+        <Input className="Input" type="text" multiline required rows={4} label="Mensagem" maxLength={400} value={this.state.multiline} onChange={() => { this.handleChange(this, 'multiline'); }} />
         <p>
-          <button className="w3-btn w3-padding" type="submit">ENVIAR</button>
+          <button className="w3-btn w3-padding" type="submit">
+            ENVIAR
+          </button>
         </p>
       </form>
     );
--- src/components/top-bar/index.jsx
+++ src/components/top-bar/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import AppBar from 'react-toolbox/lib/app_bar';
-import Navigation from 'react-toolbox/lib/navigation';
 import ToakeeIcon from '~/src/components/icon';
 
 require('./style.scss');
 
 const TopBar = () => (
-  <AppBar title="Toakee" className="TopBar w3-top  w3-card-2" leftIcon={<ToakeeIcon />}>
-    <Navigation type="horizontal" />
-  </AppBar>
+  <AppBar title="Toakee" className="TopBar w3-top  w3-card-2" leftIcon={<ToakeeIcon />} />
 );
 
 export default TopBar;
