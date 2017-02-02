e76d6d72597433f4915666182f6a8dc8b65d1905
ive <ivetourinho@gmail.com>
2017-02-01 22:40:58 -0300

    features

--- src/components/landing/features.jsx
+++ src/components/landing/features.jsx
@@ -26,10 +26,20 @@ declare var item;
 declare var idx;
 
 const Features = () => (
-  <div className="Features w3-container w3-padding-24">
-    <h3 className="w3-center">Toakee!</h3>
-    <p className="w3-center w3-large">Key features of our company</p>
-    <div className="w3-row-padding w3-center">
+  <div className="Features  bgimg-1 w3-display-container">    
+    <div className="Content w3-row">    
+      <div className="w3-col w3-container l6">    
+        <span className="Span w3-jumbo w3-hide-small">    
+          The greatest take away message!    
+        </span>    
+        <span className="Span w3-xxlarge w3-hide-large w3-hide-medium">    
+          Another take away message for mobile users!    
+        </span>    
+        <br />    
+        <span className="Span w3-large">    
+          Here more details about what is worth to keep on mind regard to our product.              
+        </span>
+      </div>
       <For each="item" in index="idx" of={features}>
         <FeatureElement
           key={idx}
--- src/components/landing/header.jsx
+++ src/components/landing/header.jsx
@@ -4,17 +4,17 @@ const Header = () => (
   <div className="Header bgimg-1 w3-display-container">
     <div className="Content w3-row">
       <div className="LeftBlock w3-col w3-container l7">
-        <span className="Span w3-jumbo w3-hide-small">
+        <div className="span w3-jumbo w3-hide-small">
           The greatest take away message!
-        </span>
-        <span className="Span w3-xxlarge w3-hide-large w3-hide-medium">
+        </div>
+        <div className="span w3-xxlarge w3-hide-large w3-hide-medium">
           Another take away message for mobile users!
-        </span>
+        </div>
         <br />
-        <span className="Span w3-large">
+        <div className="span w3-large">
           Here more details about what is worth to keep on mind regard to our product.
-        </span>
-        <p>
+        </div>
+        <p className="cta">
           <a
             href="#contact"
             className="w3-btn w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off"
@@ -22,7 +22,7 @@ const Header = () => (
             Contacte-nos!
           </a>
         </p>
-        <div className="Brands w3-xlarge">
+        <div className="brands w3-xlarge">
           <a className="Icon w3-hover-text-white">
             <i className="fa fa-facebook-official" />
           </a>
