60139444d6cde8132749d0d4823f3eba1e681205
Erik Vinicius <erik@jusbrasil.com.br>
2017-03-29 01:11:24 -0300

    Add terms of use

--- src/server/index.js
+++ src/server/index.js
@@ -33,6 +33,10 @@ app.post('/send-email', (req, res) => {
   return res.json({ ok: true });
 });
 
+app.get('/termos-de-uso', (_, res) => {
+  res.render('termos-de-uso.html');
+});
+
 app.get('/social-login', (_, res) => {
   res.render('social-login.html');
 });
