5664f1043ac98b56117a7f9f3fe221774b2103fc
Erik Vinicius <erik@jusbrasil.com.br>
2017-03-29 11:48:54 -0300

    Fix social redirect url

--- src/server/config.js
+++ src/server/config.js
@@ -3,15 +3,18 @@ import { reduce } from 'lodash';
 const env = (key, miss) => (process.env[key] || miss);
 const reducer = (obj, miss, key) => ({ ...obj, [key]: env(key, miss) });
 
+const defaultUri = 'http://localhost:3000';
+
+const PORT = env('PORT', 3000);
+const BASE_URI = env('BASE_URI', `http://localhost:${PORT}`);
+
 const config = reduce({
   GRAPHQL_URI: 'http://localhost:4000',
-  BASE_URI: 'http://localhost:3000',
-  SOCIAL_REDIRECT_URI: 'http://localhost:3000/social-login',
+  SOCIAL_REDIRECT_URI: `${BASE_URI}/social-login`,
   FACEBOOK_APP_ID: '',
   INSTAGRAM_APP_ID: '',
-  PORT: 3000,
   RP_TIMEOUT: 5000,
   SUPPORT_EMAIL: 'support@toakee.com',
-}, reducer, {});
+}, reducer, { PORT, BASE_URI });
 
 export default config;
