37692e3954870e0be2f8f07113019f1abeb1683a
ive <ivetourinho@gmail.com>
2017-05-04 21:47:18 -0300

    remove edit-list

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -13,14 +13,12 @@ import Logged from '~/src/pages/logged';
 import EventFeed from '~/src/components/event-feed';
 import EventPage from '~/src/components/event-page';
 import EventGuestList from '~/src/components/event-guest-list';
-import EventGuestListEdit from '~/src/components/event-guest-list-edit';
 
 const loggedRoutes = (
   <Route path="/" component={Logged}>
     <IndexRoute component={EventFeed} />
     <Route path="/evento/:slug" component={EventPage} />
     <Route path="/evento/:slug/lista" component={EventGuestList} />
-    <Route path="/evento/:slug/editar-listas" component={EventGuestListEdit} />
     <Route path="/redirect" onEnter={() => { window.location = '/'; }} />
   </Route>
 );
