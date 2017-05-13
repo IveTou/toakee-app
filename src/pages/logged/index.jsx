29f418f4f9873fae2f7ee31164eda6a7753d8172
ive <ivetourinho@gmail.com>
2017-05-13 01:00:02 -0300

    Prepare logged component

--- src/pages/logged/index.jsx
+++ src/pages/logged/index.jsx
@@ -1,16 +1,27 @@
 import React, { PropTypes } from 'react';
+//import { getToken } from '~/src/utils/session';
+import TrackingAPI from '~/src/toakee-core/apis/tracking';
 import TopBar from '~/src/components/top-bar';
 
 require('./style.scss');
 
-const Logged = ({ children }) => (
-  <div className="Logged mdl-layout mdl-layout--fixed-header mdl-js-layout">
-    <TopBar />
-    <main className="main mdl-layout__content">
-      {children}
-    </main>
-  </div>
-);
+class Logged extends React.Component {
+  
+  componentWillMount() {
+    //TrackingAPI.track('Logged Page View', 'pid');
+  }
+
+  render() {
+    return (
+      <div className="Logged mdl-layout mdl-layout--fixed-header mdl-js-layout">
+        <TopBar />
+        <main className="main mdl-layout__content">
+          {this.props.children}
+        </main>
+      </div>
+    );
+  }
+}
 
 Logged.propTypes = {
   children: PropTypes.node,
