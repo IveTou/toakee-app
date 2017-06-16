c0437059c2afac02f1aa5762aba27040751e5ff5
ive <ivetourinho@gmail.com>
2017-06-16 02:57:43 -0300

    Banner auto sliding

--- src/components/banner-carousel/index.jsx
+++ src/components/banner-carousel/index.jsx
@@ -11,24 +11,26 @@ if (process.env.BROWSER) {
 const settings = {
   dots: true,
   infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
-  cellSpacin: 0,
-  cellAlign: 'center',
+  cellSpacin: 20, 
 };
 
 const url = [
-  'http://placehold.it/1000x400/ffffff/c0392b/&text=slide1', 'http://placehold.it/1000x400/ffffff/c0392b/&text=slide2',
-  'http://placehold.it/1000x400/ffffff/c0392b/&text=slide3','http://placehold.it/1000x400/ffffff/c0392b/&text=slide4',
-  'http://placehold.it/1000x400/ffffff/c0392b/&text=slide5','http://placehold.it/1000x400/ffffff/c0392b/&text=slide6',
+  'https://catracalivre.com.br/wp-content/uploads/2015/10/imagens-3D_11.gif',
+  'https://cdn.mensagenscomamor.com/content/images/p000002673.jpg?v=0',
+  'https://s-media-cache-ak0.pinimg.com/originals/9d/8b/e8/9d8be883d5ee9887c6575d5561549d2e.jpg',
+  'http://marketingdeconteudo.com/wp-content/uploads/2016/08/imagens-gratis-4.jpg',
+  'https://static.tudointeressante.com.br/uploads/2013/12/imagens-aereas-surpreendentes.png',
+  'http://esquenta.com.br/wp-content/uploads/2013/09/10007959-1367035383184.jpg',
 ]
 
 const BannerCarousel = () => (
   <Slider {...settings}>
-    <div>
-      {url.map(url => <img src={url} />)}
-    </div>
+    {url.map(url => <div><img src={url} /></div>)}
   </Slider>
 );
 
