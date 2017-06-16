35165907e37faaba2a99c66933c92f97f9d6e8dc
ive <ivetourinho@gmail.com>
2017-06-16 01:20:25 -0300

    Initial commit

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -80,19 +80,12 @@ export default class Landing extends React.Component {
     return (
       <div className="Landing">
         <div className="Landing-banner">
-          <div className="Landing-banner-content">
-            <div className="Landing-banner-content-title">A solução</div>
-            <div className="Landing-banner-content-subtitle">para listas de eventos</div>
-            <p className="Landing-banner-content-body">
-              Você, promoter, publique seu evento e aproveite
-              toda praticidade que oferecemos.
-            </p>
-
-            <p className="Landing-banner-content-body right">
-              Você, baladeiro, faça aquele &quot;Vamos marcar...&quot; acontecer.
-              Confira nossos eventos!
-            </p>
-          </div>
+          <Container>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
+    pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt.
+    Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius
+    laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
+          </Container>
 
           <div className="Landing-banner-list">
             <EventList
