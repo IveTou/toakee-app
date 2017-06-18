1c4bfcaecaa6ebad5f0dd8fd0abe96d076803fe7
ive <ivetourinho@gmail.com>
2017-06-18 03:51:59 -0300

    Background and little adjusts

--- src/components/banner-carousel/index.jsx
+++ src/components/banner-carousel/index.jsx
@@ -9,12 +9,12 @@ if (process.env.BROWSER) {
 
 
 const images = [
-  'https://catracalivre.com.br/wp-content/uploads/2015/10/imagens-3D_11.gif',
-  'https://cdn.mensagenscomamor.com/content/images/p000002673.jpg?v=0',
-  'https://s-media-cache-ak0.pinimg.com/originals/9d/8b/e8/9d8be883d5ee9887c6575d5561549d2e.jpg',
-  'http://marketingdeconteudo.com/wp-content/uploads/2016/08/imagens-gratis-4.jpg',
-  'https://static.tudointeressante.com.br/uploads/2013/12/imagens-aereas-surpreendentes.png',
-  'http://esquenta.com.br/wp-content/uploads/2013/09/10007959-1367035383184.jpg',
+  'https://res.cloudinary.com/toakeeassets/image/upload/s--e0tcSGpi--/v1497419551/events/592ccf20140f0.jpg',
+  'https://res.cloudinary.com/toakeeassets/image/upload/s--hViaUCT9--/v1497743494/events/19030625_1389314694492303_6749445352138203782_n.jpg',
+  'https://res.cloudinary.com/toakeeassets/image/upload/s--TA5P9YAK--/v1494622507/events/18425168_1097932713673984_3660071346111008452_n.jpg',
+  'https://res.cloudinary.com/toakeeassets/image/upload/s---1d7OEUU--/v1497747626/events/64742e456fb347269841093e8b0813e5.jpg',
+  'https://res.cloudinary.com/toakeeassets/image/upload/s--rUjsJjXc--/v1494366866/events/Ger_C3_B4nimo_foto-de-Andre-Lima.jpg',
+  'https://res.cloudinary.com/toakeeassets/image/upload/s--tBm1oWsv--/v1497637201/events/29460_400x400.jpg',
 ];
 
 const BannerImage = (url, index) => (
