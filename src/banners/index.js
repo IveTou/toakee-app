109b46149bd7c7fd4b6a0f7f6045632ed59a2d58
ive <ivetourinho@gmail.com>
2017-06-29 16:13:15 -0300

    New banners and bg

--- src/banners/index.js
+++ src/banners/index.js
@@ -1,5 +1,7 @@
+import config from '~/src/config';
+
 export default [
-  { img: 'http://res.cloudinary.com/toakeeassets/image/upload/s--Iusw6LT6--/v1498697546/core/site/ads/img-20170627-wa0002.jpg', url: 'http://www.toakee.com/' },
-  { img: 'http://res.cloudinary.com/toakeeassets/image/upload/s--yCpsttZJ--/v1498697546/core/site/ads/img-20170627-wa0004.jpg', url: 'http://www.toakee.com/' },
-  { img: 'http://res.cloudinary.com/toakeeassets/image/upload/s--_OW9fRyk--/v1498697546/core/site/ads/img-20170627-wa0005.jpg', url: 'http://www.toakee.com/' },
+  { img: `${config.ASSETS_BASE_URI}/core/site/ads/banner-parceria.png`, url: 'http://www.toakee.com/' },
+  { img: `${config.ASSETS_BASE_URI}/core/site/ads/banner-user.png`, url: 'http://www.toakee.com/' },
+  { img: `${config.ASSETS_BASE_URI}/core/site/ads/banner-empresa.png`, url: 'http://www.toakee.com/' },
 ];
