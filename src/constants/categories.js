1d61f1a6fd1a95f7e161af24752492cc571b3828
Erik Vinicius Gomes Almeida <erik@jusbrasil.com.br>
2017-07-03 16:56:32 -0300

    Change categories order

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,5 +1,5 @@
 export default {
-  Teatro: '5907e89cb65971000fbffb10',
-  Show: '5907e89cb65971000fbffb11',
   Baladas: '590a1820cbf3d0000f704f1d',
+  Show: '5907e89cb65971000fbffb11',
+  Teatro: '5907e89cb65971000fbffb10',
 };
