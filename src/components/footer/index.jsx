d0431aab2b604cb7370fb8203ab0c756885e02b7
ive <ivetourinho@gmail.com>
2017-07-03 22:36:12 -0300

    Basic style adjusts

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button, Image } from 'semantic-ui-react';
+import { Button, Image, Form, Input, TextArea, Message } from 'semantic-ui-react';
 import FacebookProvider, { Page } from 'react-facebook';
 
 import { FACEBOOK_APP_ID, FACEBOOK_PAGE_URI } from '~/src/config';
@@ -13,6 +13,7 @@ const Footer = () => (
   <footer className="Footer mdl-mini-footer">
     <div className="Footer-row">
       <div className="Footer-row-col social">
+        <h2 className="header">Seja nosso amigo!</h2>
         <FacebookProvider appId={FACEBOOK_APP_ID}>
           <Page href={FACEBOOK_PAGE_URI} tabs smallHeader adaptContainerWidth />
         </FacebookProvider>
@@ -21,6 +22,7 @@ const Footer = () => (
       <div className="Footer-row-divider" />
 
       <ul className="Footer-row-col about">
+        <h2 className="header">Sobre Nós</h2>
         <li>
           <Button size="large" className="Button about">
             <span>Quem somos</span>
@@ -41,6 +43,46 @@ const Footer = () => (
       <div className="Footer-row-divider" />
     
       <div className="Footer-row-col form">
+        <h2 className="header">Fale Conosco</h2>
+        <Form inverted className="Form" size="large">
+          <Form.Input
+            required
+            className="Form-input"
+            label="E-mail"
+            placeholder="visitante@dominio.com"
+          />
+          <Form.Field
+            required
+            className="Form-field" 
+            id="form-textarea"
+            control={TextArea} 
+            label="Mensagem"
+            placeholder="Digite aqui sua mensagem"
+          />
+          <Message
+            success
+            header="Mensagem enviada com sucesso"
+            content="Obrigado por entrar em contato. Nos vemos em breve!"
+          />
+          <Message
+            error
+            header='Algum erro ocorreu'
+            content='Por favor, cheque sua conexão ou tente mais tarde.'
+          />
+          <Message
+            warning
+            header='Algo está incorreto'
+            list={[
+              'Por favor, cheque seu endereço de e-mail e certifique-se que a mensagem não está vazia.',
+            ]}
+          />
+          <Form.Checkbox
+            inline
+            label='Quero receber e-mails com sugestões, promoções e novidades.'
+            required
+          />
+          <Button size="large" className="Button submit"><span>Enviar</span></Button>
+        </Form>
       </div>
 
     </div> 
