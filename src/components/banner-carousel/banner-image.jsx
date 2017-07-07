4f6a537c3a263b2767684e910a3c220df4eefa79
ive <ivetourinho@gmail.com>
2017-07-07 01:13:20 -0300

    Revison fixes

--- src/components/banner-carousel/banner-image.jsx
+++ src/components/banner-carousel/banner-image.jsx
@@ -2,11 +2,11 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
 const BannerImage = ({ title, subtitle, img, url, ...slickProps }) => (
-  <Link {...slickProps} to={{ pathname: `${url}` }} >
-    <div className="BannerImage" style={{ backgroundImage: `url(${img})` }}>
+  <Link {...slickProps} to={{pathname: `${url}`}} >
+    <div className="BannerImage" style={{backgroundImage: `url(${img})`}}>
       <div className="BannerImage-content">
-        <div className="BannerImage-content-title">{ title }</div>
-        <div className="BannerImage-content-sub">{ subtitle }</div>
+        <div className="BannerImage-content-title">{title}</div>
+        <div className="BannerImage-content-sub">{subtitle}</div>
       </div>
     </div>
   </Link>
