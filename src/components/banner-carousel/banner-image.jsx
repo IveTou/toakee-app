1b97949d28685f53519a0b62026513a25bc59644
Ive Tourinho <IveTou@users.noreply.github.com>
2017-07-07 00:22:24 -0300

    Revert "Ive/design/landing/banner (#26)"
    This reverts commit 3b20016d7f3865c5f179f7934897ef9aea875143.

--- src/components/banner-carousel/banner-image.jsx
+++ src/components/banner-carousel/banner-image.jsx
@@ -1,20 +1,12 @@
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
 
-const BannerImage = ({ title, subtitle, img, url, ...slickProps }) => (
-  <Link {...slickProps} to={{ pathname: `${url}` }} >
-    <div className="BannerImage" style={{ backgroundImage: `url(${img})` }}>
-      <div className="BannerImage-content">
-        <div className="BannerImage-content-title">{ title }</div>
-        <div className="BannerImage-content-sub">{ subtitle }</div>
-      </div>
-    </div>
-  </Link>
+const BannerImage = ({ img, url, ...slickProps }) => (
+  <a {...slickProps} href={url}>
+    <div className="BannerImage" style={{ backgroundImage: `url(${img})` }} />
+  </a>
 );
 
 BannerImage.propTypes = {
-  title: PropTypes.string,
-  subtitle: PropTypes.string,
   img: PropTypes.string,
   url: PropTypes.string,
   slickProps: PropTypes.object,
--- src/constants/banners.js
+++ src/constants/banners.js
@@ -1,7 +1,7 @@
 import config from '~/src/config';
 
 export default [
-  { title: 'Somos a peça que faltava no seu negócio.', subtitle: 'Seja nosso parceiro e descubra as vantagens de ter seus eventos no melhor guia de entretenimento.', img: `${config.ASSETS_BASE_URI}/core/site/banners/banner-parceria.jpg`, url: '/' },
-  { title: 'Saiba o que está rolando na cidade!', subtitle: 'Cadastre-se e fique por dentro das novidades!', img: `${config.ASSETS_BASE_URI}/core/site/banners/banner-user.jpg`, url: '/cadastrar' },
-  { title: 'Difícil divulgar seu evento em uma cidade grande como Salvador, não é?!', subtitle: 'Publique seu evento no melhor guia de eventos, é gratuito.', img: `${config.ASSETS_BASE_URI}/core/site/banners/banner-empresa.jpg`, url: '/' },
+  { img: `${config.ASSETS_BASE_URI}/core/site/banners/banner-parceria.png`, url: '/' },
+  { img: `${config.ASSETS_BASE_URI}/core/site/banners/banner-user.png`, url: '/cadastrar' },
+  { img: `${config.ASSETS_BASE_URI}/core/site/banners/banner-empresa.png`, url: '/' },
 ];
