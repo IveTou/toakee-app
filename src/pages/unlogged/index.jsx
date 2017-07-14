ecf7726b4e0bafc20e195e603236a9020d298bef
Ive Tourinho <IveTou@users.noreply.github.com>
2017-07-14 17:35:49 -0300

    [WIP] New Track page view (#30)
    * New Track page view
    
    * Tiny fix

--- src/pages/unlogged/index.jsx
+++ src/pages/unlogged/index.jsx
@@ -1,28 +1,18 @@
 import React, { PropTypes } from 'react';
-import TrackingAPI from '~/src/toakee-core/apis/tracking';
 import TopBar from '~/src/components/top-bar';
 import Snackbar from '~/src/components/snackbar';
 
 require('./style.scss');
 
-class Unlogged extends React.Component {
-
-  componentWillMount() {
-    TrackingAPI.track('Unlogged Page View', 'Guest');
-  }
-
-  render() {
-    return (
-      <div className="Unlogged">
-        <TopBar />
-        <Snackbar />
-        <main className="main">
-          {this.props.children}
-        </main>
-      </div>
-    );
-  }
-}
+const Unlogged = ({ children }) => (
+  <div className="Unlogged">
+    <TopBar />
+    <Snackbar />
+    <main className="main">
+      {children}
+    </main>
+  </div>
+);
 
 Unlogged.propTypes = {
   children: PropTypes.node,
