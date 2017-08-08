4bc34f22515aac63f265c8b96949bb32a1bfd278
ive <ivetourinho@gmail.com>
2017-08-08 00:33:27 -0300

    Style fixes #2

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
-import classNames from 'classnames';
-
-import config from '~/src/config';
-import { deviceInfo } from '~/src/utils/device-info';
 
 if (process.env.BROWSER) {
   require('./style.scss');
 }
 
-const logoImg = deviceInfo.isDesktop
-  ? `${config.ASSETS_BASE_URI}/core/site/logo.png`
-  : '/imgs/topbar-logo.png';
-
-const classes = classNames('Logo', { 'Logo--small': !deviceInfo.isDesktop });
-
 const Logo = () => (
   <Link to={{ pathname: '/' }} >
-    <img className={classes} alt="toakee logo" src={logoImg} />
+    <img className="Logo" alt="toakee logo" />
   </Link>
 );
 
