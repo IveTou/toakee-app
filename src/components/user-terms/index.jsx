368ef6771f71487410a3aadc39f2b20d98f8526d
ive <ivetourinho@gmail.com>
2017-08-27 22:41:55 -0300

    Begining to add sticky

--- src/components/user-terms/index.jsx
+++ src/components/user-terms/index.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Grid, Header, Menu, Sticky } from 'semantic-ui-react';
+import autoBind from 'react-autobind';
 
 if (process.env.BROWSER) {
   require('./style.scss');
 }
 
-const UserTerms = () => (
-  <Container className="UserTerms" text>
-    <Header as='h2'>Termos e Condições de Uso Toakee</Header>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
-  </Container>
-);
+class UserTerms extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeItem: 'home'};
+    autoBind(this);
+  }
+
+  handleContextRef (contextRef) {
+    this.setState({ contextRef });
+  }
+
+  handleItemClick (e, { name }) {
+    this.setState({ activeItem: name });
+  }
+
+  render() {
+    const { contextRef, activeItem } = this.state;
+    console.log(contextRef);
+
+    return (
+      <div className="UserTerms" ref={this.handleContextRef}>
+        <Grid>
+          <Grid.Column width={4}>
+
+              <Menu pointing secondary vertical color="orange">
+                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+                <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+                <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+              </Menu>
+
+          </Grid.Column>
+          <Grid.Column  width={12}>
+            <Container text>
+              <Header as='h2'>Header</Header>
+            </Container>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default UserTerms;
