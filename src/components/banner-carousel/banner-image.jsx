dbb3f921cbef25008f22c53da23f862a0af4b2d4
ive <ivetourinho@gmail.com>
2017-09-11 19:29:09 -0300

    Provisory banner fix
    Provisory banner fix

--- src/components/banner-carousel/banner-image.jsx
+++ src/components/banner-carousel/banner-image.jsx
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
-const BannerImage = ({ title, subtitle, img, url, ...slickProps }) => (
-  <Link {...slickProps} to={{ pathname: `${url}` }} >
+const BannerImage = ({ title, subtitle, img, url }) => (
+  <Link to={{ pathname: `${url}` }} >
     <div className="BannerImage" style={{ backgroundImage: `url(${img})` }}>
       <div className="BannerImage-content">
         <div className="BannerImage-content-title">{title}</div>
@@ -17,7 +17,6 @@ BannerImage.propTypes = {
   subtitle: PropTypes.string,
   img: PropTypes.string,
   url: PropTypes.string,
-  slickProps: PropTypes.object,
 };
 
 export default BannerImage;
