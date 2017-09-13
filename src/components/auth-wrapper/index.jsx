ef265d6a8026c6ba0b6e16d9f594b5b4ef38d0d2
ive <ivetourinho@gmail.com>
2017-09-13 04:34:11 -0300

    Dynamic bgs

--- src/components/auth-wrapper/index.jsx
+++ src/components/auth-wrapper/index.jsx
@@ -1,5 +1,8 @@
 import React, { PropTypes } from 'react';
 import { Card } from 'semantic-ui-react';
+import autoBind from 'react-autobind';
+
+import { backgrounds } from '~/src/constants';
 
 const propTypes = {
   children: PropTypes.node,
@@ -9,15 +12,31 @@ if (process.env.BROWSER) {
   require('./style.scss');
 }
 
-const AuthWrapper = ({ children }) => (
-  <div className="AuthWrapper">
-    <Card className="AuthWrapper-content">
-      <Card.Content>
-        {children}
-      </Card.Content>
-    </Card>
-  </div>
-);
+class AuthWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+  }
+
+  renderBackgrounds(){
+    return backgrounds.map(bg =>
+      <li><span style={{ backgroundImage: `url(${bg.url})`, animationDelay:bg.delay }}/></li>
+    );
+  }
+
+  render() {
+    return (
+      <div className="AuthWrapper">
+        <ul className="slideshow">{this.renderBackgrounds()}</ul>
+        <Card className="AuthWrapper-content">
+          <Card.Content>
+            {this.props.children}
+          </Card.Content>
+        </Card>
+      </div>
+    );
+  }
+}
 
 AuthWrapper.propTypes = propTypes;
 
