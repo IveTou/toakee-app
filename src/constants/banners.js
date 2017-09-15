efa631100c20ae6b3c73450d4bd2aebca2ca977d
ive <ivetourinho@gmail.com>
2017-09-15 01:27:36 -0300

    Change path because of adblocker

--- src/constants/banners.js
+++ src/constants/banners.js
@@ -6,25 +6,25 @@ export default [
   {
     title: '',
     subtitle: '',// eslint-disable-line
-    img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/banner-parada.jpg`,
+    img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/parada.jpg`,
     url: '/evento/16-parada-lgbt-de-salvador-2017',
   },
   {
     title: 'Somos a peça que faltava no seu negócio.',
     subtitle: 'Seja nosso parceiro e descubra as vantagens de ter seus eventos no melhor guia de entretenimento.',// eslint-disable-line
-    img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/banner-parceria.jpg`,
+    img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/parceria.jpg`,
     url: '/',
   },
   {
     title: 'Saiba o que está rolando na cidade!',
     subtitle: 'Cadastre-se e fique por dentro das novidades!',
-    img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/banner-user.jpg`,
+    img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/user.jpg`,
     url: '/cadastrar',
   },
   {
     title: 'Difícil divulgar seu evento em uma cidade grande como Salvador, não é?!',
     subtitle: 'Publique seu evento no melhor guia de eventos, é gratuito.',
-    img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/banner-empresa.jpg`,
+    img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/empresa.jpg`,
     url: '/',
   },
 ];
