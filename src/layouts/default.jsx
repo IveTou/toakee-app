1f0a68c63acf7c65e31f2bc8332faa4b9c976a35
ive <ivetourinho@gmail.com>
2017-10-06 23:16:37 -0300

    Meta-tags at main

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -1,8 +1,11 @@
 import React, { PropTypes } from 'react';
+import MetaTags from 'react-meta-tags';
 import classNames from 'classnames';
 
 import TopBar from '~/src/components/top-bar';
 import Footer from '~/src/components/footer';
+
+import config from '~/src/config';
 
 if (process.env.BROWSER) {
   require('./style.scss');
@@ -14,6 +17,19 @@ const buildClasses = topbarTransparent => classNames('DefaultLayout', {
 
 const DefaultLayout = ({ children, topbarTransparent, hideFooter }) => (
   <div className={buildClasses(topbarTransparent)}>
+    <MetaTags>
+      <title>Toakee</title>
+      <meta id="og-title" property="og:title" content="Toakee - O melhor guia de eventos." />
+      <meta
+        id="og-description"
+        name="og:description"
+        content="Encontre o melhor evento da sua vida!" />
+      <meta
+        id="og-image"
+        property="og:image"
+        content="${config.ASSETS_BASE_URI}/core/site/login-bg.jpg" />
+      <meta id="og-url" property="og:url" content={location.href} />
+    </MetaTags>
     <TopBar transparent={topbarTransparent} />
     <main>{children}</main>
     <If condition={!hideFooter}>
