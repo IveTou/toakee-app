11c035e21b10e6ad4af9cae1309a6db7c3566633
ive <ivetourinho@gmail.com>
2017-10-16 00:42:08 -0300

    Remove meta-tags

--- src/pages/event/index.jsx
+++ src/pages/event/index.jsx
@@ -4,7 +4,6 @@ import { Icon, Card, Image, Grid, Button } from 'semantic-ui-react';
 import Lightbox from 'react-images';
 import classNames from 'classnames';
 import autoBind from 'react-autobind';
-import MetaTags from 'react-meta-tags';
 
 import DefaultLayout from '~/src/layouts/default';
 import { fullDateFormat, timeFormat } from '~/src/utils/moment';
@@ -119,13 +118,6 @@ export class EventPage extends React.Component {
 
     return (
       <DefaultLayout>
-        <MetaTags>
-          <title>{title}</title>
-          <meta id="og-title" property="og:title" content={title} />
-          <meta id="og-description" name="og:description" content={description} />
-          <meta id="og-image" property="og:image" content={flyer} />
-          <meta id="og-url" property="og:url" content={location.href} />
-        </MetaTags>
         <Grid columns={2} className={classes}>
           <Grid.Column className="EventPage-gallery" mobile={16} tablet={8} computer={8}>
             <Lightbox
