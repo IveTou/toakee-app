a949517e8f7206f64b7b4606049cecfba9b688b4
ive <ivetourinho@gmail.com>
2017-10-23 23:36:37 -0300

    Banners

--- src/constants/banners.js
+++ src/constants/banners.js
@@ -8,30 +8,35 @@ const banners = [
     subtitle: '',// eslint-disable-line
     img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/capa-insanno.png`,
     url: '/evento/5956f979624af5000473fa5d',
+    description: 'insanno',
   },
   {
     title: '',
     subtitle: '',// eslint-disable-line
     img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/parada.jpg`,
     url: '/evento/59af8d75c274e800046c7530',
+    description: 'parada LGBT',
   },
   {
     title: 'Somos a peça que faltava no seu negócio.',
     subtitle: 'Seja nosso parceiro e descubra as vantagens de ter seus eventos no melhor guia de entretenimento.',// eslint-disable-line
     img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/parceria.jpg`,
     url: '/quem-somos',
+    description: 'quem somos',
   },
   {
     title: 'Saiba o que está rolando na cidade!',
     subtitle: 'Cadastre-se e fique por dentro das novidades!',
     img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/user.jpg`,
     url: '/cadastrar',
+    description: 'cadastrar',
   },
   {
     title: 'Difícil divulgar seu evento em uma cidade grande como Salvador, não é?!',
     subtitle: 'Publique seu evento no melhor guia de eventos, é gratuito.',
     img: `${ASSETS_BASE_URI}/${ASSETS_BANNERS_PATH}/empresa.jpg`,
     url: '/',
+    description: 'principal',
   },
 ];
 
