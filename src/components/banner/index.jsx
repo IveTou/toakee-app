794f325e12cafcb68c4d167f3d2dd5596b8e6bcf
IveTou <ive_tourinho@hotmail.com>
2018-01-28 22:10:30 -0300

    New Banner Preparing

---
+++ src/components/banner/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ActionSchedule, MapsPlace } from 'material-ui/svg-icons';
+import { white } from 'material-ui/styles/colors';
+import { banners } from '~/src/constants';
+
+if (process.env.BROWSER) {
+  require('./style.scss');
+}
+
+declare var banner;
+
+const Banner = () => (
+  <Link className="Banner" to="/">
+    <div className="Banner-description" >
+      <h1 className="Banner-description-title">Title Title Title</h1>
+      <div className="Banner-description-calendar">
+        <div className="Banner-description-calendar-month">
+        Jan
+        </div>
+        <div className="Banner-description-calendar-day">
+         28
+        </div>
+      </div>
+      <div className="Banner-description-timeAndPlace">
+        <div className="Banner-description-timeAndPlace-place">
+          <MapsPlace color={white} /><span>place</span>
+        </div>
+        <div className="Banner-description-timeAndPlace-time">
+          <ActionSchedule color={white} /><span>17h</span>
+        </div>
+      </div>
+      <If condition={true}>
+        <h2 className="Banner-description-call">Veja como foi</h2>
+      </If>
+    </div>
+  </Link>
+);
+
+export default Banner;
