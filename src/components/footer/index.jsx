97bc94bfceb59954d5e4b5cf203c3fe3e354a1b9
ive <ivetourinho@gmail.com>
2018-02-06 23:45:46 -0300

    Column scheme

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -14,23 +14,58 @@ const Footer = ({ compressed }) => {
     return (
       <footer className={classes}>
         <div className="Footer-content">
-          <div className="Footer-content-contact">
-            <div
-              href="https://www.facebook.com/eu.toakee"
-              aria-label="facebook"
-            />
-            <div href="https://www.instagram.com/eu.toakee" aria-label="instagram"/>
-            <div href="https://linkedin.com/company/toakee" aria-label="linkedin"/>
+          <div className="Footer-content-row">
+            <div className="Footer-content-row-contact column">
+              <p>Fale conosco</p>
+              <ul>
+                <li>contato@toakee.com</li>
+                <li>+55 71 99999999</li>
+                <li>Rua parara parara, Savador-Ba</li>
+              </ul>
+            </div>
+            <div className="Footer-content-row-services column">
+              <p>Serviços</p>
+              <ul>
+                <li>
+                  <Link className="Footer-content-row-services-event" to="/quem-somos">
+                    <span>Crie seu evento</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Footer-content-row-services-signup" to="/quem-somos">
+                    <span>Cadastre-se</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Footer-content-row-services-dashboard" to="/quem-somos">
+                    <span>Meus eventos</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="Footer-content-row-about column">
+              <p>Sobre nós</p>
+              <ul>
+                <li>
+                  <Link className="Footer-link about" to="/quem-somos">
+                    <span>Quem somos?</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="Footer-link terms" to="/termos-de-uso">
+                    <span>Termos de Uso</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="Footer-content-row-social column">
+              <p>Estamos aqui</p>
+              <div href="https://www.facebook.com/eu.toakee" aria-label="facebook"/>
+              <div href="https://www.instagram.com/eu.toakee" aria-label="instagram"/>
+              <div href="https://linkedin.com/company/toakee" aria-label="linkedin"/>
+            </div>
           </div>
-          <div className="Footer-content-actions">
-            <Link className="Footer-link about" to="/quem-somos">
-              <span>Quem somos?</span>
-            </Link>
-            <Link className="Footer-link terms" to="/termos-de-uso">
-              <span>Termos de Uso</span>
-            </Link>
-          </div>
-          <div className="Footer-copyright" >
+          <div className="Footer-content-copyright" >
             Copyright &copy; 2017 Toakee. Todos os direitos reservados.
           </div>
         </div>
