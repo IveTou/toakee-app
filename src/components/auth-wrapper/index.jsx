74482881dd639237754865af104f19b53ddb96b2
IveTou <ive_tourinho@hotmail.com>
2018-03-02 22:21:06 -0300

    Auth pages workaround

--- src/components/auth-wrapper/index.jsx
+++ src/components/auth-wrapper/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'semantic-ui-react';
-import classNames from 'classnames';
 
 import { backgrounds } from '~/src/constants';
 
@@ -12,22 +11,8 @@ if (process.env.BROWSER) {
 declare var idx;
 declare var background;
 
-const AuthWrapper = ({ children, slideshow }) => (
-  <div className={classNames('AuthWrapper', { static: !slideshow })}>
-    <If condition={slideshow}>
-      <ul className="AuthWrapper-slideshow">
-        <For each="background" index="idx" of={backgrounds}>
-          <li key={idx}>
-            <span
-              style={{
-                backgroundImage: `url(${background.url})`,
-                animationDelay: background.delay,
-              }}
-            />
-          </li>
-        </For>
-      </ul>
-    </If>
+const AuthWrapper = ({ children }) => (
+  <div className="AuthWrapper static">
     <Card className="AuthWrapper-content">
       <Card.Content>
         {children}
