c1bb63b70d35390f9b584dba48904c973a495248
IveTou <ive_tourinho@hotmail.com>
2018-04-07 11:25:52 -0300

    New attend button

--- src/components/attend-button/index.jsx
+++ src/components/attend-button/index.jsx
@@ -1,44 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { compose } from 'recompose';
+import { Manager, Target, Popper } from 'react-popper';
+import { compose, withState } from 'recompose';
 import { graphql } from 'react-apollo';
-import { Button, Icon } from 'material-ui';
+import {  Button, Divider, Icon, Menu, MenuItem } from 'material-ui';
 import { withAuth } from '~/src/components/auth-modal/hoc';
 
 import query, { registerAttendance, unregisterAttendance } from './graphql';
 
-const AttendButton = ({ attendance, toggle, discountLists = [] }) => (
-  <Button.Group color="green" basic={!attendance} size="small">
-    <Choose>
-      <When condition={discountLists.length && !attendance}>
-        <Dropdown text='Vou' button icon="checkmark" className="icon">
-          <Dropdown.Menu>
-            <Dropdown.Header content='Selecione uma lista de desconto' />
-            <For each="discountList" of={discountLists}>
-              <Dropdown.Item onClick={() => toggle(discountList.id)}>
-                {discountList.name}
-              </Dropdown.Item>
-            </For>
-            <Dropdown.Item onClick={toggle}>Não colocar nome em lista</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </When>
-      <Otherwise>
-        <Button
-          icon="checkmark"
-          onClick={toggle}
-          basic={!attendance}
-          content="Vou"
-        />
-      </Otherwise>
-    </Choose>
-  </Button.Group>
-);
+const AttendButton = ({ attendance, toggle, discountLists = [], anchor, setAnchor }) => {
+  const menuItemClick = (discountListId) => {
+    setAnchor(null);
+    discountListId ? toggle(discountListId) : toggle();
+  }
+
+  return(
+    <div>
+      <Button
+        onClick={(discountLists.length && !attendance) ? e => setAnchor(e.target) : toggle}
+        color={attendance ? 'primary' : 'default'}
+        variant="raised"
+        style={{ margin: 8 }}
+      >
+        Vou
+        <Icon style={{ marginLeft: 8 }}>thumb_up</Icon>
+      </Button>
+      <Menu open={!!anchor} anchorEl={anchor} onClose={() => setAnchor(null)}>
+        <MenuItem disabled>Selecione uma lista de desconto</MenuItem>
+        <Divider light />
+        <For each="discountList" of={discountLists}>
+          <MenuItem key={discountList.id} onClick={() => menuItemClick(discountList.id)}>
+            {discountList.name}
+          </MenuItem>
+        </For>
+        <MenuItem onClick={() => menuItemClick()}>Não colocar nome em lista</MenuItem>
+      </Menu>
+    </div>
+  );
+}
 
 AttendButton.propTypes = {
   attendance: PropTypes.object,
   toggle: PropTypes.func,
   discountLists: PropTypes.arrayOf(PropTypes.object),
+  anchor: PropTypes.object,
+  setAnchor: PropTypes.func,
 };
 
 const injectData = graphql(query, {
@@ -64,10 +70,12 @@ const injectUnregisterAttendance = graphql(unregisterAttendance, {
   }),
 });
 
+const injectState = withState('anchor', 'setAnchor', null);
 
 export default compose(
   withAuth,
   injectData,
+  injectState,
   injectRegisterAttendance,
   injectUnregisterAttendance,
 )(AttendButton);
