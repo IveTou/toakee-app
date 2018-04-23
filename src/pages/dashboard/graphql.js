59cf09a1b444d6a219ce1bbb36f5b71a2eb15245
Erik Vinicius <erik@jusbrasil.com.br>
2018-04-23 00:35:56 -0300

    Add infinite scroll to event dashboard

--- src/pages/dashboard/graphql.js
+++ src/pages/dashboard/graphql.js
@@ -1,11 +1,11 @@
 import gql from 'graphql-tag';
 
 export const dashboardMenuQuery = gql`
-  query DashboardMenu($start: Date) {
+  query DashboardMenu($start: Date, $skip: Int) {
     viewer {
       id
 
-      events(start: $start, onlyMine: true) {
+      events(start: $start, onlyMine: true, skip: $skip) {
         id,
         title,
         flyer,
--- src/pages/dashboard/menu.jsx
+++ src/pages/dashboard/menu.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import { Icon } from 'semantic-ui-react';
+import { get } from 'lodash';
+import VisibilitySensor from 'react-visibility-sensor';
+import { CircularProgress } from 'material-ui';
 
 import { toggleDashboard } from '~/src/ducks/dashboard';
 
@@ -12,37 +15,95 @@ import { dashboardMenuQuery } from './graphql';
 
 declare var event;
 
-const DashboardMenu = ({ viewer = {}, open, toggle, match }) => (
-  <div className={`DashboardMenu${open ? ' DashboardMenu-open' : ''}`}>
-    <div className="DashboardMenu-header">
-      <Icon className="DashboardMenu-header-icon" name="sidebar" onClick={toggle} />
-      <span className="DashboardMenu-header-title">Gerenciar eventos</span>
-    </div>
-    <div className="DashboardMenu-events">
-      <For each="event" of={viewer.events || []}>
-        <DashboardMenuItem
-          key={event.id}
-          open={match.params.id === event.id}
-          {...event}
-        />
-      </For>
-    </div>
-  </div>
-);
+class DashboardMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchingMore: false,
+      hasMore: true,
+    };
+  }
+
+  loadMore() {
+    if (!this.state.fetchingMore) {
+      this.setState({ fetchingMore: true }, async () => {
+        const { data } = await this.props.loadMore();
+        this.setState({
+          fetchingMore: false,
+          hasMore: !!get(data, 'viewer.events.length'),
+        });
+      });
+    }
+  }
+
+  render() {
+    const { open, toggle, match, events, loading } = this.props;
+
+    return (
+      <div className={`DashboardMenu${open ? ' DashboardMenu-open' : ''}`}>
+        <div className="DashboardMenu-header">
+          <Icon className="DashboardMenu-header-icon" name="sidebar" onClick={toggle} />
+          <span className="DashboardMenu-header-title">Gerenciar eventos</span>
+        </div>
+        <div className="DashboardMenu-events">
+          <For each="event" of={events}>
+            <DashboardMenuItem
+              key={event.id}
+              open={match.params.id === event.id}
+              {...event}
+            />
+          </For>
+          <If condition={this.state.hasMore}>
+            <If condition={!loading}>
+              <VisibilitySensor
+                onChange={isVisible => (isVisible && this.loadMore())}
+              />
+            </If>
+            <div className="DashboardMenu-events-spinner">
+              <CircularProgress />
+            </div>
+          </If>
+        </div>
+      </div>
+    );
+  }
+}
 
 DashboardMenu.propTypes = {
-  viewer: PropTypes.object,
+  events: PropTypes.arrayOf(PropTypes.object),
   match: PropTypes.object,
   open: PropTypes.bool,
+  loading: PropTypes.bool,
   toggle: PropTypes.func,
+  loadMore: PropTypes.func,
 };
 
-const DashboardMenuWithData = graphql(dashboardMenuQuery, {
-  options: () => ({
-    variables: { start: moment() },
-  }),
-  props: ({ data: { viewer } }) => ({ viewer }),
-})(DashboardMenu);
+const DashboardMenuWithData = props => (
+  <Query
+    query={dashboardMenuQuery}
+    variables={{ start: moment().startOf('day') }}
+  >
+    {({ loading, fetchMore, data }) => (
+      <DashboardMenu
+        {...props}
+        events={get(data, 'viewer.events', [])}
+        loading={loading}
+        loadMore={() => fetchMore({
+          variables: { skip: get(data, 'viewer.events.length', 0) },
+          updateQuery: (previousResult, { fetchMoreResult }) => ({
+            viewer: {
+              ...previousResult.viewer,
+              events: [
+                ...get(previousResult, 'viewer.events', []),
+                ...get(fetchMoreResult, 'viewer.events', []),
+              ],
+            }
+          }),
+        })}
+      />
+    )}
+  </Query>
+);
 
 export default connect(
   ({ dashboard }) => ({ open: dashboard.get('open') }),
