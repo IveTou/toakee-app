e79b23739cb395e4604e8c2aa04c80d0baa969d4
Erik Vinicius <erik@jusbrasil.com.br>
2018-05-05 16:28:15 -0300

    Fix layout

--- src/pages/my-events/index.jsx
+++ src/pages/my-events/index.jsx
@@ -1,10 +1,9 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
 import { List, ListItem, Divider, Avatar, Paper } from 'material-ui';
 import moment from 'moment';
 import VisibilitySensor from 'react-visibility-sensor';
-
-import DefaultLayout from '~/src/layouts/default';
 
 import query from './graphql';
 
@@ -16,52 +15,49 @@ const MyEvents = ({ viewer = {}, loadMore, loading }) => {
   const { events = [] } = viewer;
 
   return (
-    <DefaultLayout hideFooter>
-      <div className="MyEvents">
-        <div className="MyEvents-section">
-          <h3 className="MyEvents-section-title">Criados por mim</h3>
-          <Paper>
-            <List className="MyEvents-section-list">
-              <For each="event" of={events} index="idx">
-                <ListItem
-                  leftAvatar={<Avatar src={event.flyer} />}
-                  primaryText={event.title}
-                  secondaryText={moment(event.start).format('DD/MM')}
-                />
-                <If condition={idx < events.length - 1}>
-                  <Divider />
-                </If>
-              </For>
-              <If condition={!loading}>
-                <VisibilitySensor onChange={isVisible => (isVisible && loadMore())} />
+    <div className="MyEvents">
+      <div className="MyEvents-section">
+        <h3 className="MyEvents-section-title">Meus eventos</h3>
+        <Paper>
+          <List className="MyEvents-section-list">
+            <For each="event" of={events} index="idx">
+              <ListItem
+                leftAvatar={<Avatar src={event.flyer} />}
+                primaryText={event.title}
+                secondaryText={moment(event.start).format('DD/MM')}
+              />
+              <If condition={idx < events.length - 1}>
+                <Divider />
               </If>
-            </List>
-          </Paper>
-        </div>
+            </For>
+            <If condition={!loading}>
+              <VisibilitySensor onChange={isVisible => (isVisible && loadMore())} />
+            </If>
+          </List>
+        </Paper>
       </div>
-    </DefaultLayout>
+    </div>
   );
+};
+
+MyEvents.propTypes = {
+  viewer: PropTypes.object,
+  loadMore: PropTypes.func,
+  loading: PropTypes.bool,
 };
 
 export default graphql(query, {
   options: () => ({
-    variables: { start: moment(), limit: 2 },
+    variables: { start: moment().startOf('day'), limit: 2 },
   }),
   props: ({ data: { viewer, fetchMore, loading } }) => ({
     loading,
     viewer,
     loadMore: () => {
       const { events = [] } = viewer;
-      const start = events.length && new Date(events[events.length - 1].start);
-
-      const skip = events.length && events
-        .filter(e => start.getTime() === new Date(e.start).getTime())
-        .length;
-
-      console.log({ start, skip });
 
       return fetchMore({
-        variables: { start, skip, strict: true },
+        variables: { skip: events.length, strict: true },
         updateQuery: (previousResult, { fetchMoreResult }) => (
           !fetchMoreResult
             ? previousResult
